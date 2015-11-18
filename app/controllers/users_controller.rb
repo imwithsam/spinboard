d26bed77e7132aef9533025c335e0f003a2cdc3b
Samson Brock <SamsonLB@gmail.com>
2015-11-18 11:20:02 -0700

    Add nav and save session upon new user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
+      session[:user_id] = user.id
       flash[:success] = "New account created for #{user.email_address}!"
       redirect_to links_path
     else
