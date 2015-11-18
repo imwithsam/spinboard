e022c8afe0e23ee6bbf8a5a0b845608081887201
Samson Brock <SamsonLB@gmail.com>
2015-11-18 13:07:30 -0700

    Add basic adding/displaying of links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,5 +2,27 @@ class LinksController < ApplicationController
   before_filter :authorize
 
   def index
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    @links = Link.all
+
+    if @link.save
+      flash.now[:success] = "#{@link.title} saved!"
+      @link = Link.new
+      render action: "index"
+    else
+      @link = Link.new
+      flash.now[:warning] = "Unable to add link."
+      render action: "index"
+    end
+  end
+
+private
+
+  def link_params
+    params.require(:link).permit(:url, :title)
   end
 end
