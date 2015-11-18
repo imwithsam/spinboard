c0253246c65b2dba61955308b35881e219e22ef7
Samson Brock <SamsonLB@gmail.com>
2015-11-18 11:31:37 -0700

    Add test for logging in an existing user

--- spec/features/user_can_log_in_or_sign_up_spec.rb
+++ spec/features/user_can_log_in_or_sign_up_spec.rb
@@ -5,10 +5,24 @@ RSpec.feature "Unauthenticated user can Log In or Sign Up", type: :feature do
     visit root_path
 
     click_link_or_button "Sign Up"
-    fill_in "Email address", with: "Jane Doe"
+    fill_in "Email address", with: "samson@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Submit"
+
+    expect(current_path).to eq(links_path)
+    expect(page).to have_selector(:link_or_button, "Sign Out")
+  end
+
+  scenario "User visits root and can log in" do
+    User.create(
+      email_address: "samson@example.com",
+      password:      "password"
+    )
+
+    visit root_path
+
+    click_link_or_button "Log In"
 
     expect(current_path).to eq(links_path)
     expect(page).to have_selector(:link_or_button, "Sign Out")
