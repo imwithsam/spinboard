7eb5fab252f03636b8c34c1b18e1d19d2dc6a087
Samson Brock <SamsonLB@gmail.com>
2015-11-19 06:24:55 -0700

    Clean up code and add Bootstrap design

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -2,6 +2,7 @@ module LinksHelper
   def read_status_url(link)
     link_to (link.read ? "Mark as Unread" : "Mark as Read"),
       link_path(id: link.id, link: { url: link.url, title: link.title, read: !link.read }),
-      remote: true, method: :patch
+      remote: true, method: :patch,
+      class: link.read ? "btn btn-info" : "btn btn-primary"
   end
 end
