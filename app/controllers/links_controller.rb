792b023ca624238be12f05caf98ba9d43693eb3e
Samson Brock <SamsonLB@gmail.com>
2015-11-18 19:16:58 -0700

    Add link status updating with AJAX

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -18,9 +18,21 @@ class LinksController < ApplicationController
     redirect_to links_path
   end
 
+  def update
+    @link = current_user.links.find(params[:id])
+
+    if @link && @link.update(title: params[:title], read: !params[:read])
+      flash[:success] = "#{@link.title} updated!"
+      render json: @link
+    else
+      flash[:warning] = "Unable to update link."
+      render json: { success: false }
+    end
+  end
+
 private
 
   def link_params
-    params.require(:link).permit(:url, :title)
+    params.require(:link).permit(:url, :title, :read)
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,12 @@ class Link < ActiveRecord::Base
   validate :url_must_be_valid
   belongs_to :user
 
+  def status
+    read ? "read" : "unread"
+  end
+
+private
+
   def url_must_be_valid
     uri = URI.parse(url)
     errors.add(:url, "must be valid") unless uri.kind_of?(URI::HTTP)
